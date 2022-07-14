1cb95900d38398d31bba0d04fdae84aa4d6a8e2a
Evan Robertson <erober7@seas.upenn.edu>
2022-07-14 01:01:36 +0000

    Restore EDR Branch

---
+++ app/src/main/java/App.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.backup.BackupClient;
+import software.amazon.awssdk.services.backup.model.BackupException;
+
+//BACKUP Plan IDs
+
+//EC2 BackupId a765a264-6cba-4c2d-a56c-42ec0d7abad3
+//RDS BackupID 6e36f3fa-d2e0-4afe-8f7f-7ab348f2851b
+
+public class App {
+
+  public static void main(String[] args) throws IOException {
+
+    try{
+
+      Region region = Region.US_EAST_1;
+      BackupClient client =  BackupClient.builder().region(region).build();
+
+      SparcRestore restore = new SparcRestore(client, "ec2sparcvault"); 
+
+      restore.restoreResource(0);
+
+      //close connection
+      client.close(); 
+
+
+   } catch(BackupException e){
+
+      System.err.println(e.awsErrorDetails().errorMessage());
+      System.exit(1); 
+   } catch (Exception e) {
+
+     System.err.println(e); 
+     System.exit(1); 
+
+  }
+  
+  }
+
+}
