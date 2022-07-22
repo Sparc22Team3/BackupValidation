07eebe9de04597eb55112e45715ce4fd3d3819a1
rushi650 <friend.rachel@gmail.com>
2022-07-22 00:38:01 -0700

    describe instnace

--- app/src/main/java/RDSRestore.java
+++ app/src/main/java/RDSRestore.java
@@ -5,26 +5,18 @@
 
 //package com.example.rds;
 
-// snippet-start:[rds.java2.describe_instances.import]
-import software.amazon.awssdk.services.backup.model.RecoveryPointByBackupVault;
-import software.amazon.awssdk.services.backup.model.StartRestoreJobRequest;
-import software.amazon.awssdk.services.backup.model.StartRestoreJobResponse;
 import software.amazon.awssdk.services.rds.RdsClient;
 import software.amazon.awssdk.services.rds.model.*;
 import software.amazon.awssdk.services.rds.model.ModifyDbInstanceRequest;
 import software.amazon.awssdk.services.rds.model.ModifyDbInstanceResponse;
 import software.amazon.awssdk.services.rds.model.RdsException;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
 
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowStateListener;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Consumer;
 
 import static java.lang.Thread.sleep;
-import static jdk.nashorn.internal.objects.NativeDate.getTime;
 
 public class RDSRestore {
 
@@ -53,43 +45,38 @@ public class RDSRestore {
 
         // If you are restoring from a shared manual DB snapshot, the DBSnapshotIdentifier must be the ARN of the shared DB snapshot.
 
-        Long startTime = System.nanoTime();
          RestoreDbInstanceFromDbSnapshotRequest request = RestoreDbInstanceFromDbSnapshotRequest
                  .builder()
-                 .dbInstanceIdentifier("database-TEST-6")
-                 .dbSnapshotIdentifier("arn:aws:rds:us-east-1:490610433117:snapshot:awsbackup:job-80f5fbc6-2e1b-ed56-3cf0-71df898be9c6")
+                 .dbInstanceIdentifier("database-TEST-0")
+                 .dbSnapshotIdentifier("arn:aws:rds:us-east-1:490610433117:snapshot:awsbackup:job-28b39632-9da3-92eb-d793-8b9a1aaec1dc")
                  //.vpcSecurityGroupIds("vpc-02926b86fed57e4e5")
-                 //.vpcSecurityGroupIds()
                  .dbSubnetGroupName("team3-sparc-db-subnet-group")
                  .build();
 
          RestoreDbInstanceFromDbSnapshotResponse response = rdsClient.restoreDBInstanceFromDBSnapshot(request);
 
-         System.out.println("vpcSecurityGroupIds : " +request.vpcSecurityGroupIds());
-         System.out.println("dbSubnetGroupName : " +request.dbSubnetGroupName());
+         // vpc security group IDs is empty; if there is one, might be able to update there in the restore rather than afterwards
+         //System.out.println("vpcSecurityGroupIds : " +request.vpcSecurityGroupIds());
+         //System.out.println("dbSubnetGroupName : " +request.dbSubnetGroupName());
 
-         System.out.println(" RESPONSE :: " + response.toString());
-         System.out.println(" INSTANCE :: " + response.dbInstance());
-         System.out.println(" RESPONSE META-DATA REQUEST ID :: " + response.responseMetadata().requestId());
-         // System.out.println(" RESPONSE META-DATA :: " + response.responseMetadata().toString());
+         //System.out.println(" RESPONSE :: " + response.toString());
+         //System.out.println(" INSTANCE :: " + response.dbInstance());
+          System.out.println(" RESPONSE META-DATA REQUEST ID :: " + response.responseMetadata().requestId());
+          System.out.println(" RESPONSE META-DATA :: " + response.responseMetadata().toString());
 
-        long elapsedTime = System.nanoTime() - startTime;
-        System.out.println(elapsedTime);
+
+
+        System.out.println("sleeping for 5 minutes now");
         sleep(300000);
 
- /**       WindowStateListener listener = new WindowStateListener() {
-            @Override
-            public void windowStateChanged(WindowEvent e) {
 
-            }
-        }
-  */
-
+        //rdsClient.waiter().waitUntilDBInstanceAvailable();
         modifyAttributes(response);
 
+        // the modified security group is reflected in console, but not with print statements below
         String identifier = response.dbInstance().dbInstanceIdentifier();
         System.out.println("identifier: " + identifier);
-        System.out.println(" RESPONSE vpc security group: " + response.dbInstance().vpcSecurityGroups());
+        System.out.println("vpc security group: " + response.dbInstance().vpcSecurityGroups());
 
         //return response1.dbInstance();
         //return response1.responseMetadata();
@@ -97,12 +84,21 @@ public class RDSRestore {
     }
 
     public void modifyAttributes(RestoreDbInstanceFromDbSnapshotResponse response) {
+        //if (response.dbInstance().dbInstanceStatus() != "available")
+
         ModifyDbInstanceRequest modifyDbRequest = ModifyDbInstanceRequest
                 .builder()
                 .dbInstanceIdentifier(response.dbInstance().dbInstanceIdentifier())
                 .vpcSecurityGroupIds("sg-078715763233fad97")
                 .build();
+
         ModifyDbInstanceResponse modifyDBResponse = rdsClient.modifyDBInstance(modifyDbRequest);
+        System.out.println(" Modified db restore, vpc security group: " + modifyDBResponse.dbInstance().vpcSecurityGroups());
+        System.out.println("dbInstanceStatus " +modifyDBResponse.dbInstance().dbInstanceStatus());
+        System.out.println(modifyDbRequest.dbSecurityGroups());
+       // if (modifyDBResponse.dbInstance().dbInstanceStatus() != "available")
+        //modifyDbRequest.vpcSecurityGroupIds().add("sg-078715763233fad97");
+        System.out.println("value of modify db response : " +String.valueOf(modifyDBResponse));
 
     }
 
@@ -122,12 +118,15 @@ public class RDSRestore {
             DescribeDbSnapshotsResponse responseS = rdsClient.describeDBSnapshots(request);
             System.out.println("RESPONSE snapshots: " +responseS.dbSnapshots());
 
-            /** List is sorted by dbSnapshotIdentifier
-             * not allowed to sort (unmodifiable list) // snapshotList.sort(null);
-             * so put in TreeMap sorted by SnapshotCreateTime */
+            // List is sorted by dbSnapshotIdentifier
+            // not allowed to sort (unmodifiable list) // snapshotList.sort(null);
+             // so put in TreeMap sorted by SnapshotCreateTime
             List<DBSnapshot> snapshotList = responseS.dbSnapshots();
+            // 0th element is not the most recent; it's a random one
+            System.out.println("id :: " +snapshotList.get(1).dbSnapshotIdentifier());
+            System.out.println("arn :: " +snapshotList.get(1).dbSnapshotArn());
 
-            System.out.println("size of snapshot list: " + snapshotList.size());
+ /**           System.out.println("size of snapshot list: " + snapshotList.size());
             for (DBSnapshot snapshot : snapshotList) {
                 System.out.println("Snapshot ARN is: "+snapshot.dbSnapshotArn());
                 System.out.println("Snapshot identifier is: " +snapshot.dbSnapshotIdentifier());
@@ -158,6 +157,7 @@ public class RDSRestore {
                 System.out.println("dbSubnetGroup " + instance.dbSubnetGroup());
 
             }
+  */
 
         } catch (RdsException e) {
             System.out.println(e.getLocalizedMessage());
@@ -165,9 +165,10 @@ public class RDSRestore {
         }
     }
 
+
     /** modified from:
      * https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/javav2/example_code/rds/src/main/java/com/example/rds/ModifyDBInstance.java */
-    public  void updateDbName(RdsClient rdsClient, String dbInstanceIdentifier) {
+/**    public  void updateDbName(RdsClient rdsClient, String dbInstanceIdentifier) {
 
         try {
             ModifyDbInstanceRequest request = ModifyDbInstanceRequest
@@ -183,6 +184,7 @@ public class RDSRestore {
             System.exit(1);
         }
     }
+ */
 
     /**
     public RecoveryPointByBackupVault getRecentRecoveryPoint(int recoveryNumber) throws Exception{
@@ -198,18 +200,37 @@ public class RDSRestore {
      */
 }
 
-//restore job
-// ?? // client.startRestoreJob(StartRestoreJobRequest.builder().build());
+//restore job NOTES
+
+/**       WindowStateListener listener = new WindowStateListener() {
+@Override
+public void windowStateChanged(WindowEvent e) {
+
+}
+}
+ */
+
+/**
+ DescribeEventSubscriptionsRequest eventRequest = DescribeEventSubscriptionsRequest
+ .builder()
+ .build();
+ DescribeEventSubscriptionsResponse eventResponse = rdsClient.describeEventSubscriptions(eventRequest);
+ List<EventSubscription> eventList = eventResponse.eventSubscriptionsList();
+ if (eventResponse.hasEventSubscriptionsList()) {
+ System.out.println("YES");
+ }
+ System.out.println("size of event list : " + eventList.size());
+ for (EventSubscription event : eventList) {
+ System.out.println(event);
+ }
+ */
+
 // RestoreDBInstanceFromDBSnapshot(nameOfDB); can that parameter be a new name b/c not replacing?
 
 // https://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/rds/model/RestoreDBInstanceFromDBSnapshotRequest.html
 // RestoreDBInstanceFromDBSnapshotRequest(String restored_DB, client.getDBInstanceIdentifier());
 
-//StartRestoreJobRequest.Builder r = StartRestoreJobRequest.builder().recoveryPointArn(String.valueOf(recoveryPoint));
-//StartRestoreJobRequest r = StartRestoreJobRequest.builder().recoveryPointArn(recoveryPoint.recoveryPointArn()).iamRoleArn(recoveryPoint.iamRoleArn())
-//      .metadata(metadata).build();
 
-// default StartRestoreJobResponse startRestoreJob(StartRestoreJobRequest startRestoreJobRequest)
 /**
  RestoreDBInstanceFromDBSnapshotRequest restoreDBInstanceRequest = new RestoreDBInstanceFromDBSnapshotRequest();
  restoreDBInstanceRequest.setDBSnapshotIdentifier("restoredDB");
@@ -220,10 +241,6 @@ public class RDSRestore {
 
 
  String arn = String.valueOf(recoveryPoint);
- RestoreDBInstanceFromDBSnapshotRequest request = new RestoreDbInstanceFromDbSnapshotRequest();
- RestoreDbInstanceFromDbSnapshotRequest.Builder restoredDB = RestoreDbInstanceFromDbSnapshotRequest.builder().dbInstanceIdentifier("restoredDB");
 
- StartRestoreJobResponse response = client.startRestoreJob((Consumer<StartRestoreJobRequest.Builder>) restoredDB);
- return response.restoreJobId();
  */
 
