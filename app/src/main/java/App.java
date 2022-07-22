b74a5967909e77b1552f77721dc7d1ebac32dcf2
Evan Robertson <erober7@seas.upenn.edu>
2022-07-22 17:50:46 +0000

    Updated app

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -4,9 +4,6 @@ import software.amazon.awssdk.services.backup.BackupClient;
 import software.amazon.awssdk.services.backup.model.BackupException;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 
-//EC2 BackupId a765a264-6cba-4c2d-a56c-42ec0d7abad3
-//RDS BackupID 6e36f3fa-d2e0-4afe-8f7f-7ab348f2851b
-
 public class App {
 
   public static void main(String[] args) throws IOException {
@@ -15,8 +12,9 @@ public class App {
 
       Region region = Region.US_EAST_1;
       BackupClient client =  BackupClient.builder().region(region).build();
+      int recoveryAttempt = 0; 
 
-      EC2Restore restore = new EC2Restore(client, "ec2sparcvault", 0); 
+      EC2Restore restore = new EC2Restore(client, "ec2sparcvault", recoveryAttempt); 
 
       String instanceId = restore.restoreEC2FromBackup();
       Ec2Client ec2Client = Ec2Client.builder().region(region).build(); 
