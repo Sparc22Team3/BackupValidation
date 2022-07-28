30191ae08523a6475e57e5393225e91191d98a51
jli293 <84085953+jli293@users.noreply.github.com>
2022-07-28 00:58:53 -0400

    Changes to S3.java and S3Validate.java

---
+++ app/src/main/java/S3.java
@@ -0,0 +1,99 @@
+import software.amazon.awssdk.core.waiters.WaiterResponse;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.backup.BackupClient;
+import software.amazon.awssdk.services.backup.model.BackupException;
+import software.amazon.awssdk.services.backup.model.DescribeRestoreJobRequest;
+import software.amazon.awssdk.services.backup.model.DescribeRestoreJobResponse;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
+import software.amazon.awssdk.services.s3.model.HeadBucketResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.waiters.S3Waiter;
+import sparc.team3.validator.restore.S3Restore;
+import sparc.team3.validator.util.InstanceSettings;
+import sparc.team3.validator.validate.S3Validate;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+
+public class S3 {
+
+  public static void main(String[] args) {
+
+    // ask user to provide S3 bucket name and S3 backup vault name
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter S3 Bucket Name: ");
+    String s3BucketName = scan.next();
+    System.out.print("Enter S3 Backup Vault Name: ");
+    String s3BackupVaultName = scan.next();
+
+    // initialize AWS objects
+    Region region = Region.US_EAST_1;
+    S3Client s3Client = S3Client.builder().region(region).build();
+    BackupClient backupClient =  BackupClient.builder().region(region).build();
+
+    //---------- TESTING VALIDATION ------------//
+//    String s3BucketName = "sparc-s3-team3-test";
+//    String s3BackupVaultName = "s3backupvault";
+//    String s3RestoredBucketName = "sparc-team3-s3-test2";
+//
+//    // initialize sparc.team3.validator.validate.S3Validate object
+//    S3Validate s3Validate1 = new S3Validate(s3Client, s3BucketName, s3RestoredBucketName);
+//
+//    // checksum validation
+//    boolean checksumCheck1 = s3Validate1.ChecksumValidate();
+//    if (checksumCheck1) {
+//      System.out.println("S3 Restore successfully validated!");
+//    } else {
+//      System.out.println("S3 Restore validation failed.");
+//    }
+//
+//    System.exit(0);
+    //---------- TESTING VALIDATION ------------//
+
+    try{
+      InstanceSettings instanceSettings = new InstanceSettings("sparc-team3-s3bucket", s3BackupVaultName, null, null);
+
+      // create a S3Restore instance
+      S3Restore s3Restore = new S3Restore(backupClient, s3Client, instanceSettings);
+
+      // use the latest recovery point for restore job - hard-coded for now, no need for user specification
+      int recoveryNumber = 0;
+
+      // start restore job
+      String restoredBucketName = s3Restore.restoreS3FromBackup(recoveryNumber);
+
+      // initialize sparc.team3.validator.validate.S3Validate object
+      S3Validate s3Validate = new S3Validate(s3Client, s3BucketName, restoredBucketName);
+
+      // checksum validation
+      boolean checksumCheck = s3Validate.ChecksumValidate();
+      if (checksumCheck) {
+        System.out.println("S3 Restore successfully validated!");
+      } else {
+        System.out.println("S3 Restore validation failed.");
+      }
+
+      backupClient.close();
+      s3Client.close();
+
+   }  catch (S3Exception | BackupException e) {
+
+      System.err.println(e.awsErrorDetails().errorMessage());
+      System.exit(1);
+
+   } catch (Exception e) {
+
+     System.err.println(e);
+     System.exit(1); 
+
+  }
+
+  // application concludes
+  System.out.println("Thank you for using our Backup Validation application. Goodbye!");
+  scan.close();
+
+  }
+
+}
---
+++ app/src/main/java/sparc/team3/validator/validate/S3Validate.java
@@ -0,0 +1,123 @@
+package sparc.team3.validator.validate;
+
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+import java.util.HashMap;
+import java.util.List;
+
+
+/**
+ * Class to validate original and restored S3 buckets
+ */
+public class S3Validate {
+
+    private final S3Client s3;
+    private final String originalBucket;
+    private final String restoredBucket;
+
+    public S3Validate(S3Client s3, String originalBucket, String restoredBucket){
+
+        this.s3 = s3;
+        this.originalBucket = originalBucket;
+        this.restoredBucket = restoredBucket;
+
+    }
+
+    /**
+     * Before validating objects, all objects need to be copied to the same bucket with the "SHA-256" checksum
+     * algorithm turned on.
+     * This function is called within the ChecksumValidate function
+     */
+    private void CopyS3Objects() {
+
+        // initialize ListObjectsRequest
+        ListObjectsRequest listObjects = ListObjectsRequest
+                .builder()
+                .bucket(restoredBucket)
+                .build();
+
+        ListObjectsResponse res = s3.listObjects(listObjects);
+        List<S3Object> objects = res.contents();
+
+        System.out.println("Preparing S3 buckets for validation...");
+
+        // iterate through each object in the bucket and perform the copy action
+        for (S3Object myValue : objects) {
+
+            // initialize CopyObjectsRequest
+            CopyObjectRequest copyObjects = CopyObjectRequest
+                    .builder()
+                    .sourceBucket(restoredBucket)
+                    .sourceKey(myValue.key())
+                    .destinationBucket(restoredBucket)
+                    .destinationKey(myValue.key())
+                    .checksumAlgorithm("SHA256") // turn on checksum for SHA256
+                    .storageClass(StorageClass.STANDARD_IA) // added in to ensure copy action passes through
+                    .build();
+
+            // copy objects
+            s3.copyObject(copyObjects);
+        }
+
+    }
+
+    /**
+     * Extract S3 objects from a given S3 bucket, and returning their key and checksum values in a HashMap
+     * @param bucketName the string name of the bucket to get the objects of
+     * @return a HashMap of S3 objects
+     */
+    private HashMap<String, String> GetS3Objects(String bucketName){
+
+        // initialize return map
+        HashMap<String, String> s3Objects = new HashMap<>();
+
+        // initialize ListObjectsRequest
+        ListObjectsRequest listObjects = ListObjectsRequest
+                .builder()
+                .bucket(bucketName)
+                .build();
+
+        ListObjectsResponse res = s3.listObjects(listObjects);
+        List<S3Object> objects = res.contents();
+
+        // retrieve the keys of the S3 objects and add them to s3Objects
+        for (S3Object myValue : objects) {
+
+            // initialize AWS GetObjectAttributesResponse object to get checksum value
+            GetObjectAttributesResponse
+                    objectAttributes = s3.getObjectAttributes(GetObjectAttributesRequest.builder().bucket(bucketName).key(myValue.key())
+                    .objectAttributes(ObjectAttributes.OBJECT_PARTS, ObjectAttributes.CHECKSUM).build());
+
+            // add S3 object key and checksum value to map
+            s3Objects.put(myValue.key(), objectAttributes.checksum().checksumSHA256());
+        }
+
+        return s3Objects;
+    }
+
+    /**
+     * Cross-check the checksum values of each S3 object in the original S3 bucket and the restored S3 bucket.
+     * If all values match, return true; otherwise, false
+     * @return a boolean value of whether the checksums match
+     */
+    public boolean ChecksumValidate() {
+
+        // copy objects to get SHA-256 checksum values
+        CopyS3Objects();
+
+        HashMap<String, String> originalObjs = GetS3Objects(originalBucket);
+        HashMap<String, String> restoredObjs = GetS3Objects(restoredBucket);
+
+        for (String key : originalObjs.keySet()){
+            if (restoredObjs.containsKey(key)){
+                // compare checksum value of the obj
+                if (!originalObjs.get(key).equals(restoredObjs.get(key))){
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
