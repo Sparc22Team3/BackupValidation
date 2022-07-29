82e3cdb8116d260f8513dd4aedc837209024f8bd
rushi650 <friend.rachel@gmail.com>
2022-07-28 21:58:08 -0700

    test db started, adding driver next

---
+++ app/src/main/java/DbTestingDummyDb.java
@@ -0,0 +1,42 @@
+import software.amazon.awssdk.services.rds.RdsClient;
+
+import java.sql.*;
+
+public class DbTestingDummyDb {
+
+    private RdsClient rdsClient;
+
+    public DbTestingDummyDb(RdsClient rdsClient) {
+        this.rdsClient = rdsClient;
+    }
+
+    boolean dbRowsTest() throws SQLException, ClassNotFoundException {
+        // Class.forName("com.mysql.cj.jdbc.Driver"); //update
+        //update password
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/database-dummy1", "admin","PASSWORD");
+        System.out.println(connection.getMetaData());
+        System.out.println(connection);
+
+
+        Statement statement;
+        statement = connection.createStatement();
+        ResultSet resultSet;
+        resultSet = statement.executeQuery(
+                "select * from designation");
+        String city, airportCode;
+        while (resultSet.next()) {
+            city = resultSet.getString("city").trim();;
+            airportCode = resultSet.getString("airportCode");
+            System.out.println("City : " + city
+                    + " AirportCode : " + airportCode);
+        }
+        resultSet.close();
+        statement.close();
+
+        connection.close();
+
+        return false;
+
+    }
+
+}
